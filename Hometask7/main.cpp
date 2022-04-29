feedbc33f9fea6eb74b0b28aa96c4599dfad070f
Daniil <danya_zaikin1010@mail.ru>
2022-04-29 04:45:43 +0300

    Completed 1st and 2nd tasks

---
+++ Hometask7/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "tree.cpp"
+
+using namespace std;
+
+int main() {
+
+    int x, menu;
+    tree_node_t* tree = NULL;
+//    Insert(tree, 5);
+//    Insert(tree, 2);
+//    Insert(tree, 7);
+//    Insert(tree, 6);
+//
+//    cout << FindMin(tree)->value << endl;
+//    DeleteByValue(tree, 7);
+
+    cout << "Enter tree elements: ";
+    while(cin.peek() != '\n') {
+
+        cin >> x;
+        Insert(tree, x);
+
+    }
+
+    cout << "\nChoose an action\n" <<
+        "1 - Insert element\n" <<
+        "2 - Delete element of the tree with inputted value\n" <<
+        "3 - Print tree elements\n" <<
+        "0 - Exit\n";
+    cin >> menu;
+
+    switch(menu) {
+
+    case 1:     // Insert
+        cout << "Input value, you want to insert: ";
+        cin >> x;
+        Insert(tree, x);
+        break;
+
+    case 2:     // Delete
+        cout << "Input value: ";
+        cin >> x;
+        DeleteByValue(tree, x);
+        break;
+
+    case 3:
+        cout << "Coming soon" << endl;
+        break;
+
+    case 0:
+        break;
+
+    default:
+        cout << "Wrong input. Try again" << endl;
+        break;
+
+    }
+
+}
---
+++ Hometask7/tree.cpp
@@ -0,0 +1,118 @@
+//
+// Created by danya on 29.04.2022.
+//
+
+#include "tree.h"
+#include <iostream>
+
+//using namespace std;
+
+typedef struct tree_node {
+
+    int value = 0;
+    tree_node_t* left = NULL;
+    tree_node_t* right = NULL;
+
+}tree_node_t;
+
+void Insert(tree_node_t*& current_tree_node, int value) {
+
+    if(current_tree_node == NULL) {
+
+        current_tree_node = (tree_node_t*)malloc(sizeof(tree_node_t));
+        current_tree_node->left = NULL;
+        current_tree_node->right = NULL;
+        current_tree_node->value = value;
+
+    }else {
+
+        if(value < current_tree_node->value) Insert(current_tree_node->left, value);
+        else if(value > current_tree_node->value) Insert(current_tree_node->right, value);
+
+    }
+
+}
+
+tree_node_t* FindMin(tree_node_t* current_tree_node) {      //ÏÎÄÀ¨Ì ÑÍÀ×ÀËÀ ÏÐÀÂÓÞ ÂÅÒÂÜ!!!
+
+    if(current_tree_node->left != NULL) return FindMin(current_tree_node->left);
+    return current_tree_node;
+
+}
+
+void DeleteByValue(tree_node_t*& current_tree_node, int value) {        // ÏÎÄÀ¨Ì ÊÎÐÅÍÜ ÑÍÀ×ÀËÀ!!!
+
+    if(current_tree_node->value == value) {         // Deleting root
+
+        if(current_tree_node->right != NULL) {
+
+            current_tree_node->value = FindMin(current_tree_node->right)->value;
+            DeleteByValue(current_tree_node->right, current_tree_node->value);
+
+        }else if(current_tree_node->left != NULL && current_tree_node->right == NULL) {
+
+            current_tree_node->value = FindMin(current_tree_node->left)->value;
+            DeleteByValue(current_tree_node->left, current_tree_node->value);
+
+        }else if(current_tree_node->left == NULL && current_tree_node->right == NULL) {
+
+            current_tree_node = NULL;
+
+        }
+
+    } else if (value < current_tree_node->value) {
+
+        if (current_tree_node->left->value == value) {           // If the next element is desired (current_left is desired)
+
+            //if((current_tree_node->left != NULL && current_tree_node->right != NULL) || (current_tree_node->right != NULL && current_tree_node->left == NULL)) {
+            if(current_tree_node->left->right != NULL) {
+
+                current_tree_node->left->value = FindMin(current_tree_node->left->right)->value;        // Replacing desired value with minimal from its right subbranch
+                DeleteByValue(current_tree_node->left->right,current_tree_node->left->value);               // Deleting element with replaced value
+
+            }else if(current_tree_node->left->left != NULL && current_tree_node->left->right == NULL) {
+
+                current_tree_node->left->value = FindMin(current_tree_node->left->left)->value;         // Replacing desired value with minimal from its left subbranch
+                DeleteByValue(current_tree_node->left->left, current_tree_node->left->value);               // Deleting element with replaced value
+
+            }else if(current_tree_node->left->left == NULL && current_tree_node->left->right == NULL) {               // When element with desired value has no kids
+
+                current_tree_node->left = NULL;                                                                      // Just delete it
+
+            }
+
+        } else {
+
+            DeleteByValue(current_tree_node->left, value);      // Moving to the next element
+
+        }
+
+    } else if (value > current_tree_node->value) {
+
+        if (current_tree_node->right->value == value) {
+
+            if(current_tree_node->right->right != NULL) {
+
+                current_tree_node->right->value = FindMin(current_tree_node->right->right)->value;      // Replacing desire value with minimal from its right subbranch
+                DeleteByValue(current_tree_node->right->right,current_tree_node->right->value);             // Deleting element with replaced value
+
+            }else if(current_tree_node->right->left != NULL && current_tree_node->right->right == NULL) {
+
+                current_tree_node->right->value = FindMin(current_tree_node->right->left)->value;       // Replacing desired value with minimal from its left subbranch
+                DeleteByValue(current_tree_node->right->left, current_tree_node->right->value);             // Deleting element with replaced value
+
+            }else if(current_tree_node->right->left == NULL && current_tree_node->right->right == NULL) {           // When element with desired value has no kids
+
+                current_tree_node->right = NULL;                                                                    // Just delete it
+
+            }
+
+        } else {
+
+            DeleteByValue(current_tree_node->right, value);     // Moving to the next element
+
+        }
+
+    }
+
+}
